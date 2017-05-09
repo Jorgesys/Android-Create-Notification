2389938d9c5bdabffc144c472528159e113246d4
Jorgesys <jorge.osorio@gmail.com>
2017-05-09 14:34:59 -0500

    Add files via upload

---
+++ app/src/main/java/com/jorgesys/notifications/MainActivity.java
@@ -0,0 +1,53 @@
+package com.jorgesys.notifications;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendNotification(getDate());
+            }
+        });
+
+    }
+
+    //Create notification
+    private void sendNotification(String message) {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("My notification")
+                        .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                        .setContentText(message);
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(001, mBuilder.build());
+    }
+
+
+    private String getDate(){
+        DateFormat dfDate = new SimpleDateFormat("yyyy/MM/dd");
+        String date=dfDate.format(Calendar.getInstance().getTime());
+        DateFormat dfTime = new SimpleDateFormat("HH:mm");
+        String time = dfTime.format(Calendar.getInstance().getTime());
+
+        return date + " " + time;
+    }
+}
